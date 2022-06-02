6620395714b922a70584b3bf99b3f3c04be8ada5
Harold Jeshua Varde <harvardii15@gmail.com>
2022-06-02 11:28:04 +0800

    adding styles + click events test

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,14 @@
 const Home = () => {
+    let name = 'Harold'
+
+    const handleClick = (e) => {
+        console.log('Hello, jeshuas', e);
+    }
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <p>{ name }</p>
+            <button onClick={handleClick}>Click me</button>.
         </div>
      );
 }
