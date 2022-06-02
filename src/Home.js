07b8a703f81ceeb8b2e41681da37d2e30c1971ab
Harold Jeshua Varde <harvardii15@gmail.com>
2022-06-02 12:09:00 +0800

    added usage of useState

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,21 @@
-const Home = () => {
-    let name = 'Harold'
+import { useState } from "react";
 
-    const handleClick = (e) => {
-        console.log('Hello, jeshuas', e);
-    }
-    return ( 
-        <div className="home">
-            <h2>Homepage</h2>
-            <p>{ name }</p>
-            <button onClick={handleClick}>Click me</button>.
-        </div>
-     );
+const Home = () => {
+	// let name = 'Harold'
+	const [name, setName] = useState('Harold')
+	const [age, setAge] = useState(25);
+
+	const handleClick = () => {
+		setName('Jeshua')
+		setAge(27)
+	}
+	return ( 
+		<div className="home">
+			<h2>Homepage</h2>
+			<p>{ name } is { age } years old</p>
+			<button onClick={handleClick}>Click me</button>
+		</div>
+	 );
 }
  
 export default Home;
