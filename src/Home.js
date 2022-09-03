ecf2d6bf1f8e2ab71a147b2443bb61bdb84f84e6
Harold Jeshua Varde <harvardii15@gmail.com>
2022-09-03 18:05:37 +0800

    lesson 19

--- src/Home.js
+++ src/Home.js
@@ -4,24 +4,33 @@ import BlogList from "./BlogList";
 const Home = () => {
 	const [blogs, setBlogs] = useState(null)
 	const [isPending, setIsPending] = useState(true)
+	const [error, setError] = useState(null)
 
 	useEffect(() => {
 		fetch('http://localhost:8000/blogs')
 			.then(res => {
+				console.log(res)
+				if (!res.ok) {
+					throw Error('could not fetch the data for that resource	')	
+				}
 				return res.json()
 			})
 			.then(data => {
 				console.log(data)
 				setBlogs(data)
 				setIsPending(false)
+				setError(null)
 			})
 			.catch(err => {
-				console.log(err.message)
+				setIsPending(false)
+				setError(err.message)
 			})
 	}, [])
 
 	return (
 		<div className="home">
+			{ error && <div>{ error }</div>}
+
 			{ isPending && <div>Loading...</div> }
 
 			{blogs && <BlogList blogs={blogs} title="All Blogs!" />}
