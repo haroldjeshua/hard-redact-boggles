0e75096e23af93c6423962226d73ead7a111fb29
Harold Jeshua Varde <harvardii15@gmail.com>
2022-09-03 17:55:50 +0800

    lesson 12-18 //old updates

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,33 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-	const [blogs, setBlogs] = useState([
-		{ title: "My new blog", body: "lorem ipsum...", author: "harold", id: 1 },
-		{ title: "Welcome blog", body: "lorem ipsum...", author: "jeshua", id: 2 },
-		{ title: "Web dev tips", body: "lorem ipsum...", author: "edrav", id: 3 }
-	])
+	const [blogs, setBlogs] = useState(null)
+	const [isPending, setIsPending] = useState(true)
+
+	useEffect(() => {
+		fetch('http://localhost:8000/blogs')
+			.then(res => {
+				return res.json()
+			})
+			.then(data => {
+				console.log(data)
+				setBlogs(data)
+				setIsPending(false)
+			})
+			.catch(err => {
+				console.log(err.message)
+			})
+	}, [])
 
 	return (
 		<div className="home">
-			<BlogList blogs={blogs} title="All Blogs!" />
+			{ isPending && <div>Loading...</div> }
+
+			{blogs && <BlogList blogs={blogs} title="All Blogs!" />}
+			{blogs && <BlogList blogs={blogs.filter((blog) => blog.author === 'harold')} title="Harold Blogs!" />}
+
+			{console.log(blogs)}
 		</div>
 	 );
 }
