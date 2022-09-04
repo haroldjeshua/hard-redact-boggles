a75ff3873d3cdc8fede071068b7b1f64a4dd9c97
Harold Jeshua Varde <harvardii15@gmail.com>
2022-09-04 22:11:53 +0800

    lesson 23: exact route match + link //oldupdate

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,14 +1,16 @@
+import { Link } from 'react-router-dom';
+ 
 const Navbar = () => {
 	return ( 
 		<nav className="navbar">
 			<h1><a className="site-name" href="/">The Harv Blog</a></h1>
 			<div className="links">
-				<a href="/">Home</a>
-				<a href="/create" style={{
+				<Link to="/">Home</Link>
+				<Link to="/create" style={{
 					color: "white",
 					backgroundColor: "#f1356d",
 					borderRadius: "8px"
-				}}>New Blog</a>
+				}}>New Blog</Link> 
 			</div>
 		</nav>
 	 );
