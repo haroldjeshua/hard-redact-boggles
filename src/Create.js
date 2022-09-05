2fa65e603aba978027b20c724583bc5260fd5363
Harold Jeshua Varde <harvardii15@gmail.com>
2022-09-05 21:16:10 +0800

    lesson 31: delete data

--- src/Create.js
+++ src/Create.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const Create = () => {
 	const [title, setTitle] = useState('')
 	const [body, setBody] = useState('')
 	const [author, setAuthor] = useState('harold')
-	const [isPending, setIsPending] = useState(false);
+	const [isPending, setIsPending] = useState(false)
+	const history = useHistory()
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
@@ -19,7 +21,9 @@ const Create = () => {
 			body: JSON.stringify(blog)
 		}).then(() => {
 			console.log('new blog added')
-			setIsPending(true)
+			setIsPending(false)
+			// history.go(-1)
+			history.push('/')
 		});
 	}
 
