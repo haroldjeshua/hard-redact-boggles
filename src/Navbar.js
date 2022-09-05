17b03931c9e1e30dd8854bf383a21cff41f245b7
Harold Jeshua Varde <harvardii15@gmail.com>
2022-09-05 18:44:59 +0800

    lesson 26 for jrs: reusing custom hook (useFetch)

--- src/Navbar.js
+++ src/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
 				<Link to="/">Home</Link>
 				<Link to="/create" style={{
 					color: "white",
-					backgroundColor: "#f1356d",
+					backgroundColor: "#6627b9",
 					borderRadius: "8px"
 				}}>New Blog</Link> 
 			</div>
